06c03f21b1eb9e644b767dc0e5f8b233aa7ab1e8
Xueyin Chen <xueyinchen@sherry.local>
2018-06-13 22:58:17 -0700

    Geo post end point

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,32 +2,63 @@
 
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-	res.render('login');
-})
+const { Pool } = require('pg')
 
-router.get('/rooms', (req, res, next) => {
-	res.render('rooms');
-})
-
-router.get('/chat', (req, res, next) => {
-	res.render('chat');
-})
-
-router.get('/setsession', (req, res, next) => {
-	req.session.favColor = "red";
-	res.send("Session set");
-})
-
-router.get('/getinfo', (req, res, next) => {
-	res.send(JSON.stringify({ a: 1, b: 2.0 }));
-
+const pool = new Pool({
+  host: 'bunnydbinstance.cw8sot6gsuk8.us-west-1.rds.amazonaws.com',
+  user: 'sherryluo',
+  password: 'Chxy1104',
+  database: 'bunnyDB',
+  port: 5432,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 })
 
 router.get('/mybag', (req, res, next) => {
-	res.send(JSON.stringify({ carrots: 16, bones: 20 }));
-	console.log("Data: " + JSON.stringify({ carrots: 10, bones: 20 }));
+	pool.query(`SELECT * from bag where user_id='${req.query.user_id}'`, (err, resq) => {
+		var map = {};
+	  	if (err) {
+		    console.log(err.stack)
+	  	} else {
+	    	resq.rows.forEach(function(mp) {
+		    	map[mp.item] = mp.item_count;
+	    	})  
+		}
+		res.send(JSON.stringify(map));
+	})
+})
 
+router.get('/nearby-posts', (req, res, next) => {
+	let query = `SELECT user_id, post_id, latitude, longitude, ST_Distance_Sphere(geo_point, ST_MakePoint(${req.query.longitude}, ${req.query.latitude})) as distance
+		FROM posts 
+		WHERE ST_Distance_Sphere(geo_point, ST_MakePoint(${req.query.longitude}, ${req.query.latitude})) <= ${req.query.meterRange};`;
+	console.log(query);
+	pool.query(query, (err, resq) => {
+		var result = [];
+	  	if (err) {
+		    console.log(err.stack);
+	  	} else {
+	    	result = resq.rows;
+		}
+		res.send(JSON.stringify(result));
+	})
+})
+
+router.post('/add-post', (req, res, next) => {
+	let query = `INSERT INTO posts(user_id, post_id, post_text, ts, latitude, longitude, geo_point) 
+		VALUES ('${req.query.user_id}', MD5(random()::text), '${req.query.post_text}', ${Date.now()}, 
+		${req.query.latitude}, ${req.query.longitude}, ST_SetSRID(ST_MakePoint(${req.query.longitude}, ${req.query.latitude}), 4326))`;
+	console.log(query);
+	pool.query(query, (err, resq) => {
+	  	if (err) {
+		    console.log(err.stack);
+		    res.send(err.stack);
+	  	} else {
+	    	res.send('New post added to database.');
+		}
+
+	})
 })
 
 router.use((req, res, next) => {
